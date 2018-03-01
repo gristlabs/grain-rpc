73ad0306710dfab58e4e978a9df7b63de84b7100
Cyprien P <cyprien.pindat@gmail.com>
2018-03-01 14:45:38 -0500

    adds hello world unit test and some config options

--- test/test_rpc.ts
+++ test/test_rpc.ts
@@ -11,7 +11,16 @@ class Calc implements ICalc {
   }
 }
 
+interface IGreet {
+  getGreeting(name: string): Promise<string>;
+}
+
+class MyGreeting implements IGreet {
+  public async getGreeting(name: string): Promise<string> { return `Hello, ${name}!`; }
+}
+
 describe("ts-rpc", () => {
+
   it("should be able to make unchecked stubs and call methods", async () => {
     const rpc = new Rpc();
     rpc.start((msg) => rpc.receiveMessage(msg));
@@ -20,4 +29,15 @@ describe("ts-rpc", () => {
     assert(stub);
     assert.equal(await stub.add(4, 5), 9);
   });
+
+  it("should support hello world without a checker", async () => {
+    const aRpc = new Rpc();
+    const bRpc = new Rpc();
+    aRpc.start((msg) => bRpc.receiveMessage(msg));
+    bRpc.start((msg) => aRpc.receiveMessage(msg));
+    aRpc.registerImpl("my-greeting", new MyGreeting());
+    const stub = bRpc.getStub<IGreet>("my-greeting");
+    assert.equal(await stub.getGreeting("World"), "Hello, World!");
+  });
+
 });
